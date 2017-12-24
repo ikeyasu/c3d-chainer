2e730c9861c4db5d5a302efd2664cb93e3f3d869
Yasuki Ikeuchi <ikeyasu@gmail.com>
2017-12-24 11:03:15 +0900

    Add convert_caffe_mode.py

--- caffe_model/README.md
+++ caffe_model/README.md
@@ -1,4 +1,4 @@
-Prerequirement
+# Prerequirement
 
 ```
 $ pip install youtube-dl
@@ -6,8 +6,30 @@ $ conda config --add channels conda-forge
 $ conda install opencv
 ```
 
+Ubuntu
+
+```
+$ sudo apt-get install protobuf-compiler
+```
+
+MacOSX
+
+```
+$ brew install protobuf
+```
+
+# Downloading
+
 ```
 $ wget http://vlg.cs.dartmouth.edu/c3d/conv3d_deepnetA_sport1m_iter_1900000
 $ wget https://raw.githubusercontent.com/gtoderici/sports-1m-dataset/master/labels.txt
+$ wget https://raw.githubusercontent.com/facebook/C3D/master/C3D-v1.0/src/caffe/proto/caffe.proto
 $ youtube-dl -f mp4 https://www.youtube.com/watch?v=dM06AMFLsrc -o dM06AMFLsrc.mp4
+```
+
+# Compiling caffe.proto
+
+```
+$ sed -i '1s/^/syntax = "proto2";\n/' caffe.proto # inserting syntax version on the head.
+$ protoc --python_out=. caffe.proto
 ```
--- convert_caffe_model.py
+++ convert_caffe_model.py
@@ -3,6 +3,8 @@ import os
 import numpy as np
 import pickle
 
+from chainer import serializers
+
 import models.VGG
 import models.C3D
 import chainer.links as L
@@ -25,6 +27,7 @@ p.ParseFromString(open('caffe_model/conv3d_deepnetA_sport1m_iter_1900000', 'rb')
 
 for i in conv_layers_indx:
     layer = p.layers[i]
+    print(layer.name)
     weights_b = np.array(layer.blobs[1].data, dtype=np.float32)
     weights_p = np.array(layer.blobs[0].data, dtype=np.float32).reshape(
         layer.blobs[0].num,
@@ -33,52 +36,22 @@ for i in conv_layers_indx:
         layer.blobs[0].height,
         layer.blobs[0].width,
         )
-    #print(weights_b.shape)
-    #print(model.predictor[layer.name].conv.b.shape)
     np.copyto(model.predictor[layer.name].conv.W.data, weights_p)
     np.copyto(model.predictor[layer.name].conv.b.data, weights_b)
 
 for i in fc_layers_indx:
     layer = p.layers[i]
+    name = layer.name.replace('-1', '')
+    print(name)
     weights_b = np.array(layer.blobs[1].data, dtype=np.float32)
     weights_p = np.array(layer.blobs[0].data, dtype=np.float32).reshape(
             layer.blobs[0].num,
             layer.blobs[0].channels,
             layer.blobs[0].length,
             layer.blobs[0].height,
-            layer.blobs[0].width)#[0,0,0,:,:].T
-    name = layer.name.replace('-1', '')
-    print(name)
-    print(weights_p.shape)
-    print(model.predictor[name].W.shape)
-    print(weights_b.shape)
-    print(model.predictor[name].b.shape)
+            layer.blobs[0].width)
     np.copyto(model.predictor[name].W.data, weights_p)
     np.copyto(model.predictor[name].b.data, weights_b)
 
-with open('caffe_model/labels.txt', 'r') as f:
-  labels = [line.strip() for line in f.readlines()]
-print('Total labels: {}'.format(len(labels)))
-
-import cv2
-cap = cv2.VideoCapture('caffe_model/dM06AMFLsrc.mp4')
-
-vid = []
-while True:
-  ret, img = cap.read()
-  if not ret:
-    break
-  vid.append(cv2.resize(img, (171, 128)))
-vid = np.array(vid, dtype=np.float32)
-x = vid[2000:2016, 8:120, 30:142, :].transpose((3, 0, 1, 2))
-output = model.predictor(np.array([x]))
-
-pos = np.argmax(output.data, axis=1)[0]
-print('Position of maximum probability: {}'.format(pos))
-print('Maximum probability: {:.5f}'.format(max(output.data[0])))
-print('Corresponding label: {}'.format(labels[pos]))
-
-# sort top five predictions from softmax output
-top_idxs = np.argsort(output.data[0])[::-1][:5]  # reverse sort and take five largest items
-print('\nTop 5 probabilities and labels:')
-_ = [print('{:.5f} {}'.format(output.data[0][i], labels[i])) for i in top_idxs]
+print('saving the model')
+serializers.save_npz('mlp.model', model)
